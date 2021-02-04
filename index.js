ad674192849811a749400b88b18d1e135819fdb5
iosmonbekov <iosmonbekov666@gmail.com>
2021-02-04 22:48:29 +0600

    plugin is ready

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const modal = $.modal({
     width: '400px',
     footerButtons: [
         {text: 'Ok', type: 'primary', handler(){
-            console.log('Primary btn clicked')
+            modal.close()
         }},
         {text: 'Cancel', type: 'danger', handler(){
-            console.log('Danger btn clicked')
+            modal.close()
         }}
     ]
 });
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,9 +1,25 @@
+Element.prototype.appendAfter = function(element){
+    element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop(){}
+
 function _createModalFooter(buttons = []){
-    if (buttons.length){
+    if (buttons.length === 0){
         return document.createElement('div')
     }
     const wrap = document.createElement('div')
     wrap.classList.add('modal-footer')
+
+    buttons.forEach(btn => {
+        const $btn = document.createElement('button')
+        $btn.textContent = btn.text
+        $btn.classList.add('btn')
+        $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+        $btn.onclick = btn.handler || noop
+
+        wrap.appendChild($btn)
+    })
 
     return wrap
 }
@@ -27,7 +43,7 @@ const _createModal = (options) => {
         </div>
     `)
     const footer = _createModalFooter(options.footerButtons)
-    
+    footer.appendAfter(modal.querySelector('[data-content]'))
     document.body.append(modal)
     return modal
 }
